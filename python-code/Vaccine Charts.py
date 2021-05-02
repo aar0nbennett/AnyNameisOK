14e531b6a3cd76323997c8a41f99c792f134a80c
Tanim Pavel <tanimpavel7@gmail.com>
2021-05-02 12:52:53 -0400

    add vaccine page and fix vaccine code

--- python-code/Vaccine Charts.py
+++ python-code/Vaccine Charts.py
@@ -10,13 +10,15 @@ import matplotlib.pyplot as plt
 ### Data Source ###
 # https://www.cdc.gov/flu/prevent/vaccine-supply-historical.htm
 
-fig = plt.figure()
-ax = fig.add_axes([0,0,1,1])
-ax.set(ylabel='Flu Vaccinations in Millions',
-       title='America Flu Vaccinations since 2008')
+fig, ax = plt.subplots()
+#ax = fig.add_axes([0,0,1,1])
 year = ["2008", "2009", "2010", "2011", "2012", "2013","2014","2015", "2016","2017","2018", "2019", "2020"]
 fluVaccineCount = [110.9, 114, 155.1, 132, 134.9, 134.5, 147.8, 146.4, 145.9, 155.3, 169.1, 174.5, 189.5]
 ax.bar(year,fluVaccineCount)
+fig.savefig("vaccineBar.png")
+ax.set(ylabel='Flu Vaccinations in Millions',
+       title='America Flu Vaccinations since 2008')
+
 plt.show()
 
 
@@ -31,6 +33,8 @@ ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90, )
 
 ax1.axis('equal')
 print('America Flu Vaccination Population Percentages')
+ax1.set(title='America Flu Vaccinations Population Pecentages')
+fig.savefig("pieChart.png")
 plt.show()
 
 
